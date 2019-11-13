994b08e2b5c3fa7086e11e8bc36859fe567294d0
crazy_mad <crazymad0601@gmail.com>
2019-11-13 13:37:32 +0800

    点击聚焦到所在行

--- src/pages/components/edtior03/index.tsx
+++ src/pages/components/edtior03/index.tsx
@@ -8,6 +8,7 @@ interface Props {}
 interface Line {
   inlines: string[];
   content: string;
+  selection?: number[]
   // height: 0;
 }
 
@@ -17,13 +18,13 @@ const canvas: any = document.createElement('canvas');
 const ctx: any = canvas.getContext('2d');
 
 const Edtior: React.FC<Props> = props => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<any>(null);
   const [inputContent, setInputContent] = useState();
   const [cursorScreenCoor, setCursorScreenCoor] = useState({ x: 0, y: 0 });
   const [cursorCoor, setCursorCoor] = useState<number[]>([0, 0, 2]);
   const [lines, setLines] = useState<Line[]>([
     {
-      inlines: ['超级大本营'],
+      inlines: ['超级大本营', 'dadas'],
       content: '超级大本营',
     },
     {
@@ -36,10 +37,16 @@ const Edtior: React.FC<Props> = props => {
     },
   ]);
 
+  // useEffect(() => {
+  //   setLines(lines.map((line: Line) => {
+  //     return line;
+  //   }));
+  // }, []);
+
   useEffect(() => {
     let content = lines[cursorCoor[0]].inlines[cursorCoor[1]].substring(0, cursorCoor[2]);
     let x = EditorUtils.getTextWidthByCanvas(content);
-    let y = 0;
+    let y = cursorCoor[1] * LINE_HEIGHT;
     for (let i = 0; i < cursorCoor[0]; i++) {
       y += lines[i].inlines.length * LINE_HEIGHT;
     }
@@ -49,7 +56,9 @@ const Edtior: React.FC<Props> = props => {
     });
   }, [cursorCoor]);
 
-  function toFocus() {}
+  function toFocus() {
+    inputRef.current.focus();
+  }
 
   function checkKey(e: any) {
     let coor: number[] = cursorCoor;
@@ -75,22 +84,79 @@ const Edtior: React.FC<Props> = props => {
           // 当前行的屏幕行最后一行
           coor[0] += 1;
           coor[1] = 0;
-          coor[2] = Math.min(lines[coor[0]].inlines[0].length, coor[2])
-        } else if (coor[1] < line.inlines.length) {
-          
+          coor[2] = Math.min(lines[coor[0]].inlines[0].length, coor[2]);
+        } else if (coor[1] < line.inlines.length - 1) {
+          coor[1] += 1;
+          coor[2] = Math.min(line.inlines[coor[1]].length, coor[2]);
+        } else {
+          coor[2] = line.inlines[line.inlines.length - 1].length;
         }
         break;
       case 37: // left
+        coor[2] -= 1;
+        if (coor[2] < 0) {
+          if (coor[1] === 0 && coor[0] === 0) {
+            coor[2] = 0;
+          } else if (coor[1] > 0) {
+            coor[1] -= 1;
+            coor[2] = line.inlines[coor[1]].length;
+          } else if (coor[1] === 0 && coor[0] > 0) {
+            coor[0] -= 1;
+            coor[1] = lines[coor[0]].inlines.length - 1;
+            coor[2] = lines[coor[0]].inlines[coor[1]].length;
+          }
+        }
         break;
       case 39: // right
+        coor[2] += 1;
+        if (coor[2] > line.inlines[coor[1]].length) {
+          if (coor[0] === lines.length - 1 && coor[1] === line.inlines.length - 1) {
+            coor[2] = line.inlines[coor[1]].length;
+          } else if (coor[1] < line.inlines.length - 1) {
+            coor[1] += 1;
+            coor[2] = 0;
+          } else if (coor[0] < lines.length - 1 && coor[2] > line.inlines[coor[1]].length) {
+            coor[0] += 1;
+            coor[1] = 0;
+            coor[2] = 0;
+          }
+        }
         break;
     }
+    setCursorCoor(coor.concat([]));
   }
 
   function onInput() {}
 
+  function onClickDown (e: any) {
+    let event: any = window.event;
+    let {layerX, layerY} = event;
+    let pageRowCount: number = layerY / LINE_HEIGHT;
+    let rowCount = 0;
+    for (let i = 0, len = lines.length; i < len; i++) {
+      if (rowCount + lines[i].inlines.length * LINE_HEIGHT >= layerY) {
+        let inlineIndex: number = Math.ceil((layerY - rowCount) / LINE_HEIGHT) - 1;
+        cursorCoor[0] = i;
+        cursorCoor[1] = inlineIndex;
+        if (layerX > EditorUtils.getTextWidthByCanvas(lines[i].inlines[inlineIndex])) {
+          layerX = EditorUtils.getTextWidthByCanvas(lines[i].inlines[inlineIndex]);
+        }
+        break;
+      }
+      rowCount += lines[i].inlines.length * LINE_HEIGHT;
+    }
+
+    setCursorCoor(cursorCoor.concat([]));
+  }
+
   return (
-    <div className="editor-container" onFocus={toFocus} tabIndex={-1}>
+    <div
+      className="editor-container"
+      onFocus={toFocus}
+      tabIndex={-1}
+      onMouseDown={onClickDown}
+      onMouseUp={() => {}}
+    >
       <textarea
         ref={inputRef}
         className="editor-real-input"
